2f631fdaa369fc2ccb811482b2eafe7ece755fae
SalmanAd01 <salmanadhikari415@gmail.com>
2023-03-08 12:56:21 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:my_app/realtime_db.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FirebaseApp firebaseApp = await Firebase.initializeApp(
+    options: const FirebaseOptions (
+  apiKey: "AIzaSyBmzfh8f_XFMy6flkL-obeFA1HeYxhuiuY",
+  authDomain: "first-109d7.firebaseapp.com",
+  projectId: "first-109d7",
+  storageBucket: "first-109d7.appspot.com",
+  messagingSenderId: "919364557544",
+  appId: "1:919364557544:web:866f58be580803458ec75d"
+    )
+  );
+  runApp(const MaterialApp(
+    home: realtime_db(),
+  ));
+}
---
+++ lib/realtime_db.dart
@@ -0,0 +1,53 @@
+import 'dart:html';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class realtime_db extends StatefulWidget {
+  const realtime_db({super.key});
+
+  @override
+  State<realtime_db> createState() => _realtime_dbState();
+}
+
+class _realtime_dbState extends State<realtime_db> {
+
+  late DatabaseReference _dbref;
+  String databasejson = "";
+
+  @override
+  void initState(){
+    super.initState();
+    _dbref = FirebaseDatabase.instance.reference();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Database: "+databasejson),
+              ),
+              TextButton(
+                onPressed: () {
+                  _createDB();
+                },
+                child: const Text("Create DB")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _createDB() {
+    _dbref.child("profile").set("my value");
+    _dbref
+          .child("jobprofile")
+          .set({'website':"www.siesgst.com", "website2":"www.siesedu.com"});
+  }
+}
